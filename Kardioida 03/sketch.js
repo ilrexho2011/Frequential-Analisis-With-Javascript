b1fdfac708b641eb44a5798dfc2a1b3e50e7ed6d
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2023-04-05 05:34:29 +0200

    Add files via upload

---
+++ Kardioida 03/sketch.js
@@ -0,0 +1,55 @@
+let N;
+let N_min = 10;
+let N_max = 300;
+let M;
+const M_min = 2;
+const M_max = 10;
+const R = 300;
+
+let points = [];
+
+function setup() {
+    createCanvas(800, 800);
+}
+
+function draw() {
+    background(51);
+
+    M = (map(mouseY, 0, height, M_min, M_max, true));
+    N = floor(map(mouseX, 0, width, N_min, N_max, true));
+
+    let h = height / (M_max - M_min);
+    for (let i = 0; i < M_max - M_min; i++) {
+        fill(255, 10 * i);
+        noStroke();
+        rect(0, i * h, width, h);
+        fill(255);
+        textAlign(RIGHT);
+        text(i + M_min - 1, width - 10, (i + 1) * h - 10);
+    }
+
+    translate(width / 2, height / 2);
+
+    points = [];
+    for (let i = 0; i < N; i++) {
+        let x = R * cos(TWO_PI * i / N);
+        let y = R * sin(TWO_PI * i / N);
+        let p = createVector(x, y);
+        points.push(p);
+    }
+
+    for (let i = 0; i < points.length; i++) {
+        const p = points[i];
+        const p_next = points[floor(i * M) % N];
+
+        // POINTS
+        stroke(255);
+        strokeWeight(5);
+        point(p.x, p.y);
+
+        // LINES
+        stroke(255, 150);
+        strokeWeight(2);
+        line(p.x, p.y, p_next.x, p_next.y);
+    }
+}
